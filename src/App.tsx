031024e451e09b96a5f3c3f3765a029e6dea5721
sundram <ram322222@gmail.com>
2025-10-08 21:56:57 +0530

    intialised the app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import dotenv from 'dotenv'
+function App() {
+
+dotenv.config();
+  return (
+    <div className="justify-center text-3xl">
+          astrotalk
+    </div>
+  )
+}
+
+export default App
